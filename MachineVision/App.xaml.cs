aad489e1438809c65ca74a1d290c491b0ac09e82
ningyilang <1982849947@qq.com>
2025-07-05 08:27:19 +0800

    添加项目文件。

---
+++ MachineVision/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Prism.DryIoc;
+using Prism.Ioc;
+using System;
+using System.Windows;
+
+namespace MachineVision
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
